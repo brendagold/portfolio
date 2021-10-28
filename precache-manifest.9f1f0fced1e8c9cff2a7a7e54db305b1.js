66ed41e6f864f2be88aa9c596b0df6760fd40d8b
brendagold <petersbrenda47@gmail.com>
2021-10-28 07:29:03 -0400

    Updates

---
+++ precache-manifest.9f1f0fced1e8c9cff2a7a7e54db305b1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8ac03a6823b48bdcc23202659b8eab",
+    "url": "https://devmary.online/index.html"
+  },
+  {
+    "revision": "01f2a40a3150f33dfdb4",
+    "url": "https://devmary.online/static/css/2.24b83f32.chunk.css"
+  },
+  {
+    "revision": "c91f4759a84fac0abb6f",
+    "url": "https://devmary.online/static/css/main.368f3580.chunk.css"
+  },
+  {
+    "revision": "01f2a40a3150f33dfdb4",
+    "url": "https://devmary.online/static/js/2.87bd4547.chunk.js"
+  },
+  {
+    "revision": "24b8054ddc1445462862104fe5fc876e",
+    "url": "https://devmary.online/static/js/2.87bd4547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91f4759a84fac0abb6f",
+    "url": "https://devmary.online/static/js/main.f4186715.chunk.js"
+  },
+  {
+    "revision": "aa7a445ca99b1f4dc356",
+    "url": "https://devmary.online/static/js/runtime-main.65f5e698.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "https://devmary.online/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "https://devmary.online/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "https://devmary.online/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "https://devmary.online/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "https://devmary.online/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
